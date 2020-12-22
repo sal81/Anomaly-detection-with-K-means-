54fe8aea157c01c0f6329dee03277ad9f5636887
Shalini Sil <shalinisil81@gmail.com>
2020-12-22 23:28:10 +0530

    Update seg.m

--- seg.m
+++ seg.m
@@ -1,18 +1,20 @@
 %Segmentation using k maps
-x=imread('glioblastoma.jpg');
+x=imread('brain5.jpg');
 x1=double(rgb2gray(x));
- H = fspecial('average',3);
+ H = fspecial('average',5);
  x1= imfilter(x1,H,'replicate'); 
 % x1= medfilt2(x1);
-x11=x1(:);%Coversion to matrix
+x11=x1(:);%Conversion to matrix
 copy=x1;
-[R,C,Z]=size(x1);
+[R,C,Z,K]=size(x1);
 %Centroid matrix
-mu=[55 160 215];
+mu=[50 110 180 210];
 %Centrois zeros for each cluster
 a1=zeros(R,C);
 a2=zeros(R,C);
 a3=zeros(R,C);
+a4=zeros(R,C);
+%a5=zeros(R,C);
 while(true)
     oldmu=mu;
     for i=1:R
@@ -28,6 +30,12 @@ while(true)
             if mn(1)==3;
                 a3(i,j)=x1(i,j);
             end
+            if mn(1)==4;
+                a4(i,j)=x1(i,j);
+            end
+            %if mn(1)==5;
+                %a5(i,j)=x1(i,j);
+            %end
         end
     end
             col1=a1(:);
@@ -50,12 +58,28 @@ while(true)
             len3=length(f3);
             mm3=s3/len3;
             mm33=round(mm3);%new center element
+            
+            col4=a4(:);
+            s4=sum(col4);
+            f4=find(col4);%$to find non zero elwment
+            len4=length(f4);
+            mm4=s4/len4;
+            mm44=round(mm4);%new center element
+            
+            %col5=a5(:);
+            %s5=sum(col5);
+            %f5=find(col5);%$to find non zero elwment
+            %len5=length(f5);
+            %mm5=s5/len5;
+            %mm55=round(mm5);%new center element
+            
+            
             %new centroid
-            mu=[mm11 mm22 mm33];
+            mu=[mm11 mm22 mm33 mm44];
             if (mu==oldmu)
                 break
             end
-end
+          end
 
 
 
@@ -72,10 +96,16 @@ end
                 if a3(i,j)>0
                     a3(i,j)=3;
                 end
+                if a4(i,j)>0
+                    a4(i,j)=4;
+                end
+                %if a5(i,j)>0
+                 %   a5(i,j)=5;
+                %end
             end
             end
             %clustered image
-            clusterfinal=[a1+a2+a3];
+            clusterfinal=[a1+a2+a3+a4];
 
              
              figure;imshow(x)
@@ -83,7 +113,6 @@ end
              
              figure;imshow(clusterfinal,[])
              title('Clustered Image');
-%             
                 
             
                 
